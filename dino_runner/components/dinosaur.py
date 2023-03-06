55dadb17e57046534afb855636da86e78f54b067
milosDrake <126831442+milosDrake@users.noreply.github.com>
2023-03-06 14:34:35 -0500

    Add dinosaur

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,47 @@
+import pygame
+from dino_runner.utils.constants import JUMPING, RUNNING, DUCKING
+from pygame.sprite import Sprite
+
+class Dinosaur(Sprite):
+    X_POS = 80
+    Y_POS = 310
+
+    def __init__(self):
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step = 0
+
+    def update(self, user_input):
+        if user_input[pygame.K_DOWN]:
+            self.duck()
+        elif user_input[pygame.K_UP]:
+            self.jump()    
+        else:
+            self.run()    
+        
+        self.step += 1
+        if self.step == 10:
+            self.step = 0
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+
+    def run(self):
+        self.image = RUNNING[0] if self.step < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS     
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS + 33
+
+    def jump(self):
+        self.image = JUMPING
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS - 150
