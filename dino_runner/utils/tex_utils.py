e11d734cabf2f9ad925fcbf6f38ffba4885ef678
Jhon <alexbe.beta@gmail.com>
2023-03-09 00:29:30 -0500

    add dinoScore

---
+++ dino_runner/utils/tex_utils.py
@@ -0,0 +1,11 @@
+import pygame
+
+FONT_STYLE = 'freesansbold.ttf'
+BLACK_COLOR = (0, 0, 0)
+
+def get_score_element(points):
+    font = pygame.font.Font(FONT_STYLE, 20)
+    text = font.render(f"Points: {points}", True, BLACK_COLOR)
+    text_rect = text.get_rect()
+    text_rect.center = (1000, 10)
+    return text, text_rect
